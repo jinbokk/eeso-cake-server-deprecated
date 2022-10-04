47f8391cd3498cbbd200271ae9af284685d02b90
JINBOK LEE <eyelash1024@naver.com>
2022-10-04 18:39:15 +0900

    api for cake category

--- routes/api.js
+++ routes/api.js
@@ -5,9 +5,45 @@ const Product = require("../models/product");
 router.get("/", async (req, res) => {
   try {
     const allProductsData = await Product.find({});
-    res.send(allProductsData)
+    res.send(allProductsData);
   } catch (error) {
-    res.send(error)
+    res.send(error);
+    console.log(error);
+  }
+});
+
+router.get("/rice", async (req, res) => {
+  try {
+    const riceProductsData = await Product.find({
+      ingredient: "rice",
+    });
+    res.send(riceProductsData);
+  } catch (error) {
+    res.send(error);
+    console.log(error);
+  }
+});
+
+router.get("/bread", async (req, res) => {
+  try {
+    const breadProductsData = await Product.find({
+      ingredient: "bread",
+    });
+    res.send(breadProductsData);
+  } catch (error) {
+    res.send(error);
+    console.log(error);
+  }
+});
+
+router.get("/tart", async (req, res) => {
+  try {
+    const tartProductsData = await Product.find({
+      ingredient: "tart",
+    });
+    res.send(tartProductsData);
+  } catch (error) {
+    res.send(error);
     console.log(error);
   }
 });
